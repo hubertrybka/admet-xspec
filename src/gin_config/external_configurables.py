90de97e33346c95033968c0bb3a96650e5224c06
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2025-04-18 20:40:48 +0200

    Update external_configurables.py

--- src/gin_config/external_configurables.py
+++ src/gin_config/external_configurables.py
@@ -1,5 +1,8 @@
 from chemprop import nn, featurizers
+from scipy import stats
 import gin
+
+# Chemprop external configurables
 
 gin.external_configurable(nn.AtomMessagePassing, module="nn")
 gin.external_configurable(nn.BondMessagePassing, module="nn")
@@ -11,3 +14,9 @@ gin.external_configurable(nn.NormAggregation, module="nn")
 gin.external_configurable(
     featurizers.SimpleMoleculeMolGraphFeaturizer, module="featurizers"
 )
+
+# Scipy and sklearn external configurables
+
+gin.external_configurable(stats.uniform, module="stats")
+gin.external_configurable(stats.norm, module="stats")
+gin.external_configurable(stats.lognorm, module="stats")
