f82cc0edbeb2f18f5c2c3e2f522273e6da3eeb13
kon-mtal <konradmstlr@gmail.com>
2025-10-15 22:39:26 +0200

    Add real values to classes transformation

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,6 @@ RDLogger.DisableLog("rdApp.*")
 _RdkitUncharger = Uncharger()
 _RdkitSaltRemover = SaltRemover()
 
-
 def get_clean_smiles(smiles: str, remove_salt: bool = True) -> str | None:
     """
     Strips salts and removes charges from a molecule. Returns SMILES in canonical form.
@@ -26,7 +25,7 @@ def get_clean_smiles(smiles: str, remove_salt: bool = True) -> str | None:
             return None
         if remove_salt:
             mol = SaltRemover().StripMol(mol)
-            mol = _RdkitUncharger.un.uncharge(mol)  # (*) except for this
+            mol = _RdkitUncharger.un.uncharge(mol) # (*) except for this
             # leave only the largest fragment
             mol_fragments = Chem.GetMolFrags(mol, asMols=True)
             if len(mol_fragments) > 0:
@@ -37,7 +36,6 @@ def get_clean_smiles(smiles: str, remove_salt: bool = True) -> str | None:
     else:
         return None
 
-
 def get_nice_class_name(obj):
     """
     Takes an object of any class and returns a clean name of the class.
@@ -45,7 +43,6 @@ def get_nice_class_name(obj):
     :return: str
     """
     return type(obj).__name__
-
 
 def get_metric_callable(metric_name: str):
     metrics_dict = {
@@ -63,6 +60,55 @@ def get_metric_callable(metric_name: str):
         raise ValueError
     return metrics_dict[metric_name]
 
+def get_converted_unit(
+        val: float,
+        from_unit: str,
+        to_unit: str,
+        mol_weight: float | None = None,
+        tissue_density: float | None = 1,
+    ):
+    """Convert a measurment from one unit to another."""
+    to_unit_implemented = {"uM"}
+    assert to_unit in to_unit_implemented, f"Conversion to unit {to_unit} not implemented yet."
+
+    if to_unit == "ug ml-1":
+        to_unit = "ug/ml"
+    if to_unit == "umol/L":
+        to_unit = "uM"
+
+    if to_unit == "uM":
+        assert mol_weight is not None, (
+            f"Molecular weight not provided "
+            f"for {from_unit}->{to_unit} conversion."
+        )
+
+        match from_unit:
+            case "ng/g":
+                return (val * tissue_density / mol_weight) / 1000
+            case "ug/g":
+                return val / mol_weight
+            case "ng/ml":
+                return (val / mol_weight) / 1000
+            case "ug/ml":
+                return val / mol_weight
+            case "ug/L":
+                return (val / mol_weight) / 1000
+            case "mg%":
+                mg_per_liter = val * 10
+                return (mg_per_liter * 1000) / mol_weight
+            case "nM":
+                return val / 1000
+            case "uM":
+                return val
+            case "nmol/g":
+                return val * tissue_density / 1000
+            case "pmol/g":
+                return val * tissue_density / 1_000_000
+            case "umol/Kg":
+                return val * tissue_density / 1000
+            case _:
+                logging.info(f"Do not support conversion from {from_unit}, returning None.")
+                return None
 
 def log_markdown_table(dictionary: dict):
     """
