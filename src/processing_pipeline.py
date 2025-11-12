95f055f3fa70be938d5009ba00fa307149f43d24
kon-mtal <konradmstlr@gmail.com>
2025-11-12 13:21:31 +0100

    Implemented 'train on manually provided splits' for mix-n-match

--- src/processing_pipeline.py
+++ src/processing_pipeline.py
@@ -1,4 +1,5 @@
 import gin
+import pandas as pd
 from datetime import datetime
 
 from src import PredictorBase
@@ -25,8 +26,22 @@ class ProcessingPipeline:
         reducer: ReducerBase,
         splitter: DataSplitterBase,
         predictor: PredictorBase,
-        datasets: list[str],
+        datasets: list[str],  # friendly_name
+        train_sets: list[str],  # friendly_name
+        test_sets: list[str],  # friendly_name
     ):
+        assert (datasets and not train_sets and not test_sets) or (
+            not datasets and train_sets and test_sets
+        ), (
+            "Invariant violation. Either the friendly names of datasets to be cleaned, featurized and loaded "
+            "can be provided, or the friendly names of datasets to be featurized and loaded can be provided. "
+        )
+        if not datasets:
+            assert train_sets and test_sets, (
+                "Invariant violation. When providing already-split .csvs manually, both"
+                "train and test must be specified for 'self.train_sets', 'self.test_sets'."
+            )
+
         self.model_name = model_name
 
         self.do_load_datasets = do_load_datasets
@@ -44,33 +59,37 @@ class ProcessingPipeline:
         self.datasets = datasets
 
     def run(self):
-        if self.do_load_datasets or self.do_load_train_test or self.do_train_model:
-
-            dataset_dfs = self.load_datasets()
+        if self.do_load_datasets:
+            dataset_dfs = self.load_datasets(self.datasets)
             featurized_dataset_dfs = self.featurize_datasets(dataset_dfs)
 
-        if self.do_visualize_datasets or self.do_load_train_test or self.do_train_model:
-
+        if self.do_visualize_datasets:
             self.visualize_datasets(featurized_dataset_dfs)
 
-        if self.do_load_train_test or self.do_train_model:
+        # TODO: sort out unecessary "if manual split then we have conditional behaviour in get_train_test
+        # TODO: which also duplicates the process of loading and featurizing datasets" complexity present here
+        # idea: make 'run' call a different function corresponding 1:1 with each 'plan' present in 'configs/plans' (?)
+        if self.do_load_train_test and self.do_load_datasets:
+            train_df, test_df = self.get_train_test(
+                {"featurized_dataset_dfs": featurized_dataset_dfs}
+            )
+        elif self.do_load_train_test and (self.train_sets and self.test_sets):
+            train_df, test_df = self.get_train_test(
+                {"train_sets": self.train_sets, "test_sets": self.test_sets}
+            )
 
-            train_df, test_df = self.get_train_test(featurized_dataset_dfs)
-
-        if self.do_visualize_train_test or self.do_train_model:
-
+        if self.do_visualize_train_test:
             self.visualize_train_test(train_df, test_df)
 
         if self.do_train_model:
-
             self.train_model(train_df, test_df)
             self.save_model()
             self.save_metrics()
 
-    def load_datasets(self):
+    def load_datasets(self, friendly_names: list[str]) -> list[pd.DataFrame]:
         dataset_dfs = [
             self.data_interface.get_by_friendly_name(friendly_name)
-            for friendly_name in self.datasets
+            for friendly_name in friendly_names
         ]
 
         return dataset_dfs
@@ -111,7 +130,18 @@ class ProcessingPipeline:
             )
         )
 
-    def get_train_test(self, featurized_dataset_dfs): ...
+    def get_train_test(
+        self, source_dict: dict[str, list]
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Returns a tuple of final (clean, featurized) train and test set.
+        If 'featurized_dataset_dfs' in source_dict.keys, then splitting is applied (no featurization).
+        Else, when 'train_sets' and 'test_sets' in source_dict.keys, then featurization is applied (no splitting).
+        """
+        if "featurized_dataset_dfs" in source_dict.keys():
+            ...
+        elif "train_sets" in source_dict.keys() and "test_sets" in source_dict.keys():
+            ...
 
     def visualize_train_test(self, train_df, test_df): ...
 
