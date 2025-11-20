3f17a61e03159eac384b3b21c300b993118dbee0
Hubert <hubert.rybka@doctoral.uj.edu.pl>
2025-11-20 15:00:57 +0100

    add debugging message

--- src/data/filter.py
+++ src/data/filter.py
@@ -51,24 +51,15 @@ class TanimotoFilter(FilterBase):
     ) -> pd.DataFrame:
         test_smiles = filter_against_df["smiles"].tolist()
         tc = TanimotoCalculator(smiles_list=test_smiles, featurizer=self.featurizer)
-
         results = tc.run_batch(queries=to_filter_df["smiles"].tolist())
-
-        # handle failed tanimoto queries
-        n_before_tanimoto = len(results["min_distance"])
-        mask = [x is not None for x in results["min_distance"]]
-        to_filter_df = to_filter_df[mask].copy()
-        to_filter_df["min_tanimoto"] = [
-            x for x in results["min_distance"] if x is not None
-        ]
-
-        logging.info(
-            f"Dropped {n_before_tanimoto - len(to_filter_df)} rows due to failed Tanimoto queries"
-        )
-
+        to_filter_df["min_tanimoto"] = results["min_tanimoto"]
         to_filter_df = to_filter_df[
             to_filter_df["min_tanimoto"] > self.min_distance_to_test_post_filtering
         ]
+        logging.info(
+            f"Dropped {len(results) - len(to_filter_df)} molecules after filtering by minimal \
+                            Tanimoto distance of {self.min_distance_to_test_post_filtering} to a test set."
+        )
 
         to_filter_df.drop(columns=["min_tanimoto"], inplace=True)
         return to_filter_df
